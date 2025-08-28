00dc3398f3be5724ae8d3d39263e9f83cd4aaa88
Divesh Nikam <divesh.nikam@gmail.com>
2025-08-28 17:04:12 +0530

    Update Mongo connection handling

--- db/db.connect.js
+++ db/db.connect.js
@@ -4,11 +4,17 @@ require("dotenv").config()
 const mongoUri = process.env.MONGODB
 
 const initDatabse = async () => {
-    await mongoose.connect(mongoUri).then(() => {
-        console.log("Database is connected")
-    }).catch((error) => {
-        console.log("Error connecting to database")
-    })
-}
+  try {
+    const connection = await mongoose.connect(mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    if (connection) {
+      console.log("connected succesfully");
+    }
+  } catch (error) {
+    console.log("connection failed", error);
+  }
+};
 
 module.exports = {initDatabse}
